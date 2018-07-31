237829f21746642fef481699eb9132dd96a5f58d
CHENCHAO <CHENCHAO@DESKTOP-P9FGRO2>
2018-07-31 18:34:28 +0800

    hello

---
+++ activemqAPI/src/main/java/com/activemq/hello/MqHello.java
@@ -0,0 +1,18 @@
+package com.activemq.hello;
+/**
+ * activitymq  say helloworld
+ * @author CHENCHAO
+ *
+ */
+public class MqHello {
+
+	//步骤1导入activemq架包
+	/*<dependency>
+	<groupId>org.apache.activemq</groupId>
+	<artifactId>activemq-all</artifactId>
+	<version>5.14.5</version>
+</dependency>*/
+	
+	//步骤2 
+	
+}
